dcdca8749b0fdb31fcccce665ea585f02986edbb
Pragya Kansal <pragyakansal419@gmail.com>
2023-10-16 03:29:19 +0000

    Fixed logic for reading from text file

--- src/main/scala/imperative/modular/CountTopWords.scala
+++ src/main/scala/imperative/modular/CountTopWords.scala
@@ -7,8 +7,9 @@ import scala.annotation.targetName
 import scala.io.Source
 
 trait topWords extends Task {
-  private def processLines(howMany: Int, minLength: Int, lastNWords: Int, lines: Iterator[String | Null]): Unit = {
+  private def processLines(howMany: Int, minLength: Int, lastNWords: Int, k_steps: Int, minFrequency: Int, lines: Iterator[String | Null]): Unit = {
     val slidingQueue = mutable.Queue[String]()
+    var stepCounter = 0
     import scala.language.unsafeNulls
 
     for (line <- lines) {
@@ -25,10 +26,13 @@ trait topWords extends Task {
         }
         if (slidingQueue.size > lastNWords) {
           val removedWord = slidingQueue.dequeue()
-          val wordFreq = slidingQueue.groupBy(identity).view.mapValues(_.size)
-          val topWords = wordFreq.toList.sortBy { case (_, freq) => -freq }.take(howMany)
-          val stats = topWords.map { case (word, freq) => s"$word: $freq" }.mkString(" ")
-          println(stats)
+          if (stepCounter % k_steps == 0) {
+            val wordFreq = slidingQueue.groupBy(identity).view.mapValues(_.size)
+            val topWords = wordFreq.toList.sortBy { case (_, freq) => -freq }.take(howMany)
+            val stats = topWords.map { case (word, freq) => s"$word: $freq" }.mkString(" ")
+            println(stats)
+          }
+          stepCounter += 1
         }
       }
     }
@@ -39,26 +43,33 @@ trait topWords extends Task {
     val howMany = Try(args(0).toInt).getOrElse(10)
     val minLength = Try(args(1).toInt).getOrElse(6)
     val lastNWords = Try(args(2).toInt).getOrElse(1000)
-    processLines(howMany, minLength, lastNWords, input)
+    val k_steps = Try(args(3).toInt).getOrElse(10)
+    val minFrequency = Try(args(4).toInt).getOrElse(1)
+    processLines(howMany, minLength, lastNWords, k_steps, minFrequency, input)
+ 
   }
 }
 
 object topWordsMain extends Main with topWords {
   override def main(args: Array[String]): Unit = {
-    if (args.length == 1 && args(0).endsWith(".txt")) {
-      val input = Source.fromFile(args(0)).getLines()
-      run(Seq("10", "6", "1000"))(input)
+    if (args.length < 3) {
+      println("Usage: sbt 'runMain imperative.modular.topWordsMain howMany minLength lastNWords [inputFileName]'")
     } else {
-        val howMany = Try(args(0).toInt).getOrElse(10)
-        val minLength = Try(args(1).toInt).getOrElse(6)
-        val lastNWords = Try(args(2).toInt).getOrElse(1000)
-
+      val howMany = Try(args(0).toInt).getOrElse(10)
+      val minLength = Try(args(1).toInt).getOrElse(6)
+      val lastNWords = Try(args(2).toInt).getOrElse(1000)
+      val k_steps = Try(args(3).toInt).getOrElse(10)
+      val minFrequency = Try(args(4).toInt).getOrElse(1)
+      if (args.length > 5) {
+        val inputFileName = args(5)
+        // Read input from the specified file
+        val input = scala.io.Source.fromFile(inputFileName).getLines()
+        run(Seq(howMany.toString, minLength.toString, lastNWords.toString, k_steps.toString, minFrequency.toString))(input)
+      } else {
         // Read input from standard input
         val input = scala.io.Source.stdin.getLines()
-        run(Seq(howMany.toString, minLength.toString, lastNWords.toString))(input)
-    }    
+        run(Seq(howMany.toString, minLength.toString, lastNWords.toString, k_steps.toString, minFrequency.toString))(input)
+      }
+    }
   }
-} 
-
-
-
+}
